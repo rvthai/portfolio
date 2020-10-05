bcbf99c844179057916622c455c7de32e0a99579
Ricky Thai <36805556+rvthai@users.noreply.github.com>
2020-10-04 20:02:25 -0700

    Style experience and adjust typography and padding

--- src/components/about.js
+++ src/components/about.js
@@ -7,39 +7,45 @@ function About() {
   return (
     <div id="about" className={styles.about}>
       <div className={styles.heading}>
-        <span className={styles.title}>About me</span>
+        <span className={styles.title}>
+          {" "}
+          <span style={{ color: "#f1bb52" }}>#</span> About me
+        </span>
         <div className={styles.hr} />
       </div>
       <div className={styles.content}>
         <div className={styles.leftside}>
           <p className={styles.text}>
-            <p>
+            <p style={{ marginBottom: "1rem" }}>
               Hello! My name is Ricky, a software engineer based in San Jose,
               CA. I am a recent graduate from the University of California,
               Irvine where I received a bachelor's degree in Computer Science.{" "}
             </p>
-            <p>
+            <p style={{ marginBottom: "1rem" }}>
               {" "}
               Shortly after I graduated, I discovered a passion for front-end
               development. I enjoy designing and developing applications that
               bring forth both delightful and functional experiences for people.{" "}
             </p>
-            <p>
+            <p style={{ marginBottom: "1rem" }}>
               Currently, I am working on my craft and am looking for more
               opportunities to do so.{" "}
             </p>
-            <p>
+            <p style={{ marginBottom: "2rem" }}>
               Some tools and technologies I've been working with are:
-              {/* <ul>
-              <li>React</li>
-              <li>Node</li>
-              <li>JavaScript</li>
-              <li>HTML/CSS</li>
-              <li>Gatsby</li>
-              <li>Python</li>
-              <li>Express</li>
-            </ul> */}
             </p>
+            <div className={styles.lists}>
+              <ul>
+                <li>JavaScript (ES6+)</li>
+                <li>React</li>
+                <li>Node.js</li>
+              </ul>
+              <ul>
+                <li>HTML & CSS</li>
+                <li>Python</li>
+                <li>GraphQL</li>
+              </ul>
+            </div>
           </p>
         </div>
         <div className={styles.rightside}>
--- src/components/experience.js
+++ src/components/experience.js
@@ -3,11 +3,37 @@ import styles from "../styles/experience.module.scss"
 
 function Experience() {
   return (
-    <div id="experience" style={{ marginBottom: "20rem" }}>
+    <div id="experience" className={styles.container}>
       <div className={styles.heading}>
-        <span className={styles.title}>Where I've worked</span>
+        <span className={styles.title}>
+          <span style={{ color: "#f1bb52" }}>#</span> Where I've worked
+        </span>
         <div className={styles.hr} />
       </div>
+      <div className={styles.content}>
+        <div className={styles.job}>
+          <div className={styles.line1}>
+            <span className={styles.company}>ITTIA</span>
+            <span className={styles.date}>July 2019 - July 2020</span>
+          </div>
+          <span className={styles.position}>Junior Software Engineer</span>
+        </div>
+        <div className={styles.job}>
+          <div className={styles.line1}>
+            <span className={styles.company}>Spetcial</span>
+            <span className={styles.date}>February 2019 - June 2019</span>
+          </div>
+          <span className={styles.position}>Software Developer Intern</span>
+        </div>
+        <div className={styles.job}>
+          <div className={styles.line1}>
+            <span className={styles.company}>Dreams for Schools</span>
+            <span className={styles.date}>March 2018 - June 2018</span>
+          </div>
+          <span className={styles.position}>Junior Software Engineer</span>
+        </div>
+      </div>
+      <button className="button">View my resume</button>
     </div>
   )
 }
