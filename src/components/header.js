7760390b920a1c6391b6b4202d3ca82b6fb1ceb4
Ricky Thai <rickyvanthai@gmail.com>
2020-10-05 23:05:45 -0700

    Animate navbar on scroll

--- src/components/header.js
+++ src/components/header.js
@@ -8,9 +8,29 @@ import { faBars } from "@fortawesome/free-solid-svg-icons"
 import Logo from "../images/logo.png"
 
 function Header() {
+  var prevScrollpos = window.pageYOffset
+  window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset
+    if (currentScrollPos === 0) {
+      document.getElementById("navbar").style.boxShadow = "none"
+      document.getElementById("navbar").style.height = "100px"
+      document.getElementById("navbar").style.backgroundColor = "rgb(33,33,33)"
+    } else if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar").style.top = "0"
+      document.getElementById("navbar").style.boxShadow =
+        "0 10px 30px -10px black"
+      document.getElementById("navbar").style.height = "75px"
+      document.getElementById("navbar").style.backgroundColor =
+        "rgba(33, 33, 33, 0.95)"
+    } else {
+      document.getElementById("navbar").style.top = "-100px"
+    }
+    prevScrollpos = currentScrollPos
+  }
+
   return (
-    <div className={styles.container}>
-      <div className={styles.header}>
+    <div id="navbar" className={styles.container}>
+      <div id="header" className={styles.header}>
         <Link className={styles.logo} to="/">
           <img src={Logo} alt="logo" width="35" />
         </Link>
